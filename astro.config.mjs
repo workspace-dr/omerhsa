e6820199e29b5e8eda38ec2e2f9fe2ef8cdff39c
workspace-dr <retreageiam@gmail.com>
2026-03-09 14:14:30 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwindcss';
-import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-site: 'https://workspace-dr.github.io',
+  site: 'https://workspace-dr.github.io',
   base: '/omerhsa',
-  integrations: [react(), tailwind(), sitemap()],
+  
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), vue(), svelte()],
+  
+  // Esto desactiva Sharp para evitar el error de instalación
   image: {
     service: passthroughImageService()
   }
